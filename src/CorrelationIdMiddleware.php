683600e49bef70927e77f8ba9df60e4bd8470aec
Sebastien Malot <smalot@hubside.com>
2019-01-08 12:14:48 +0100

    Support multiple headers / Allow to override them

--- src/CorrelationIdMiddleware.php
+++ src/CorrelationIdMiddleware.php
@@ -16,19 +16,47 @@ use Woody\Http\Server\Middleware\MiddlewareInterface;
 class CorrelationIdMiddleware implements MiddlewareInterface
 {
 
+    // Attribute name.
     const ATTRIBUTE_NAME = 'correlation-id';
-
-    // Generic Header.
-    const HEADER_CORRELATION_ID = 'X-Correlation-ID';
 
     // CloudFlare ID.
     const HEADER_CF_RAY = 'CF-RAY';
 
+    // Generic Header.
+    const HEADER_CORRELATION_ID = 'X-Correlation-ID';
+
+    // Request ID.
+    const HEADER_REQUEST_ID = 'X-Request-ID';
+
+    /**
+     * @var array
+     */
+    protected $headers;
+
+    /**
+     * @var string
+     */
+    protected $responseHeader;
+
     /**
      * CorrelationIdMiddleware constructor.
+     *
+     * @param array $headers
+     * @param string|false $responseHeader
      */
-    public function __construct()
+    public function __construct(array $headers = [], $responseHeader = null)
     {
+        // Default values, order matter.
+        if (empty($headers)) {
+            $headers = [
+                static::HEADER_CF_RAY,
+                static::HEADER_CORRELATION_ID,
+                static::HEADER_REQUEST_ID,
+            ];
+        }
+
+        $this->headers = $headers;
+        $this->responseHeader = $responseHeader ?? static::HEADER_CORRELATION_ID;
     }
 
     /**
@@ -46,19 +74,42 @@ class CorrelationIdMiddleware implements MiddlewareInterface
      * @param \Psr\Http\Server\RequestHandlerInterface $handler
      *
      * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Exception
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if ($request->hasHeader(self::HEADER_CF_RAY)) {
-            $correlationId = $request->getHeader(self::HEADER_CF_RAY);
-        } elseif ($request->hasHeader(self::HEADER_CORRELATION_ID)) {
-            $correlationId = $request->getHeader(self::HEADER_CORRELATION_ID);
-        } else {
-            $correlationId = Uuid::uuid4()->toString();
+        // Extract correlation id from request.
+        $correlationId = $this->getCorrelationId($request);
+
+        // Attach correlation id to request for other middleware.
+        $request = $request->withAttribute(static::ATTRIBUTE_NAME, $correlationId);
+
+        // Dispatch request to other middleware.
+        $response = $handler->handle($request);
+
+        // Return response with correlation id added.
+        if ($this->responseHeader) {
+            return $response->withHeader($this->responseHeader, $correlationId);
         }
 
-        $response = $handler->handle($request->withAttribute(self::ATTRIBUTE_NAME, $correlationId));
+        // Return without added header.
+        return $response;
+    }
 
-        return $response->withHeader(self::HEADER_CORRELATION_ID, $correlationId);
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     *
+     * @return string
+     * @throws \Exception
+     */
+    protected function getCorrelationId(ServerRequestInterface $request): string
+    {
+        foreach ($this->headers as $header) {
+            if ($request->hasHeader($header)) {
+                return $request->getHeaderLine($header);
+            }
+        }
+
+        return Uuid::uuid4()->toString();
     }
 }
